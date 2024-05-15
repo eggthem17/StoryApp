2b97ee84bdb7d2cd348c080a3c39e363b5e52140
eggthem17 <paxmiler@gmail.com>
2024-05-15 18:14:46 +0900

    Initial Commit

---
+++ StoryApp/StoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryAppApp.swift
+//  StoryApp
+//
+//  Created by MinKyu CHA on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct StoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
