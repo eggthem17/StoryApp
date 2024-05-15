af8556f800d82df48f12a887ee5e210d757cd4fe
eggthem17 <paxmiler@gmail.com>
2024-05-15 21:48:13 +0900

    + ADD: add image assets

---
+++ StoryApp/Views/Book.swift
@@ -0,0 +1,18 @@
+//
+//  Book.swift
+//  StoryApp
+//
+//  Created by MinKyu CHA on 5/15/24.
+//
+
+import SwiftUI
+
+struct Book: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Book()
+}
