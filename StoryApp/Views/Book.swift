26ef0b5a644dde78550f99c51dfcea5672981f9d
eggthem17 <paxmiler@gmail.com>
2024-05-15 22:02:10 +0900

    + ADD: BookCover Design

--- StoryApp/Views/Book.swift
+++ StoryApp/Views/Book.swift
@@ -9,7 +9,32 @@ import SwiftUI
 
 struct Book: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(._01)
+            .resizable()
+            .scaledToFill()
+            .frame(width: 176, height: 260)
+            .clipShape(.rect(cornerRadius: 12))
+            .overlay(alignment: .bottom) {
+                UnevenRoundedRectangle(bottomLeadingRadius: 12, bottomTrailingRadius: 12)
+                    .frame(height: 40)
+                    .foregroundStyle(LinearGradient(gradient: Gradient(colors: [.black, .clear]), startPoint: /*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/, endPoint: /*@START_MENU_TOKEN@*/.trailing/*@END_MENU_TOKEN@*/))
+                Text("Book Name")
+                    .bold()
+                    .foregroundStyle(.white)
+                    .frame(width: 150, alignment: .leading)
+            }
+            .overlay(alignment: .topTrailing) {
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                        .foregroundStyle(.yellow)
+                    Text("5.0")
+                        .foregroundStyle(.black)
+                }
+                .font(.system(size: 11))
+                .frame(width: 45, height: 22)
+                .background(.white, in: .rect(cornerRadius: 20))
+                .padding(12)
+            }
     }
 }
 
